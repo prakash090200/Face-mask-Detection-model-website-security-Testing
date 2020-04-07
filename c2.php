bb6cab19754fa342de0ef77fe5552f242e7bfe84
Prakash <59432256+prakash090200@users.noreply.github.com>
2020-04-07 23:38:58 +0530

    Add files via upload

---
+++ c2.php
@@ -0,0 +1,20 @@
+
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','prakash');
+$db=mysqli_select_db($con,'prakashk');
+$un=$_POST['reg'];
+$ps=$_POST['pass'];
+$b="select * from studentsignup where Registration_no='$un' && password='$ps'";
+$check=mysqli_query($con,$b);
+$num=mysqli_num_rows($check);
+if($num==1){
+	$_SESSION['registration']=$un;
+   header('location:welcome.php');
+   }
+else
+{
+	header('location:signup.php');
+}
+?>
+
