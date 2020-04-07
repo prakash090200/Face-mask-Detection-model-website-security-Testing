f6347d84344f6102797f3a2664afef21b9b82c32
Prakash <59432256+prakash090200@users.noreply.github.com>
2020-04-07 22:45:58 +0530

    Delete c3.php

--- c3.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$ok=$_POST['reg'];
-$new=$_POST['pass'];
-$con=mysqli_connect('localhost','root','prakash');
-$db=mysqli_select_db($con,'prakashk');
-$sql=mysqli_query($con," update studentsignup set password='$new' where Registration_no ='$ok' ");
-header('location:login.php');
-?>
