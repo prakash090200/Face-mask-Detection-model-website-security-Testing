424679f59a25b809d5078925b4775fb19b688dd9
Prakash <59432256+prakash090200@users.noreply.github.com>
2020-04-07 23:45:37 +0530

    Add files via upload

---
+++ faculty_backend.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','prakash');
+$db=mysqli_select_db($con,'prakashk');
+$name=$_POST['name'];
+$un=$_POST['re'];
+$ps=$_POST['pass'];
+$b="select * from faculties where reg='$un' && pass='$ps' &&name='$name' ";
+$check=mysqli_query($con,$b);
+$num=mysqli_num_rows($check);
+if($num==1){
+	$_SESSION['reg']=$un;
+	$_SESSION['name']=$name;
+   header('location:faculty_welcome_page.php');
+   }
+else
+{
+	header('location:faculty_login.php');
+}
+?>
